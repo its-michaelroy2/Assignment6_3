6803f1d9b97c2e9e44ac94d039f73e8a19b87555
its-michaelroy <72664547+its-michaelroy@users.noreply.github.com>
2024-12-11 19:37:59 -0500

    Assignment 6.3

---
+++ Assignment6_3_/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Assignment6_3_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Assignment6.3");
+            Console.WriteLine("=============\n");
+
+            Queue<Call> queue = new Queue<Call>();
+            //queue.Enqueue(new Call { Id = 1, Number = "123-456-7890", Date = DateTime.Now });
+            queue.Enqueue(new Call());
+            queue.Dequeue();
+
+        }
+        public class Call
+        {
+            public int Id { get; set; }
+            public string Number { get; set; }
+            public DateTime Date { get; set; }
+        }
+    }
+}
